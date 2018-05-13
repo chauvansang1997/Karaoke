95120be402e87d8c5fa3f1d400308f4908df90ab
Chau Van Sang <chauvansang97@gmail.com>
2018-05-13 16:38:34 +0700

    add entity

---
+++ DTO/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Customer
+    {
+        private String name;
+        private String phoneNumber;
+        private String address;
+        private String id;
+
+        public string Name { get => name; set => name = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Address { get => address; set => address = value; }
+        public string Id { get => id; set => id = value; }
+
+
+    }
+}
---
+++ DTO/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Room
+    {
+        private String name;
+        private String status;
+        //private String 
+    }
+}
