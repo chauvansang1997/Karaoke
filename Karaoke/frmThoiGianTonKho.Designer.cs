b3fd8960a60afec735772f9918f156b1b4fb289e
Chau Van Sang <chauvansang97@gmail.com>
2018-06-03 20:05:38 +0700

    commit to merge

--- Karaoke/frmThoiGianTonKho.Designer.cs
+++ Karaoke/frmThoiGianTonKho.Designer.cs
@@ -32,6 +32,7 @@
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnXem = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -53,24 +54,40 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(62, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Từ ngày";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 122);
+            this.label2.Location = new System.Drawing.Point(63, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Đến ngày";
+            // 
+            // btnXem
+            // 
+            this.btnXem.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnXem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnXem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnXem.Image = global::Karaoke.Properties.Resources.plus1;
+            this.btnXem.Location = new System.Drawing.Point(132, 160);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(143, 30);
+            this.btnXem.TabIndex = 187;
+            this.btnXem.Text = "Xem báo cáo";
+            this.btnXem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnXem.UseVisualStyleBackColor = false;
             // 
             // frmThoiGianTonKho
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(373, 225);
+            this.Controls.Add(this.btnXem);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker2);
@@ -88,5 +105,6 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnXem;
     }
 }
