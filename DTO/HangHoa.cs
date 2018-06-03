abac78045c54002fe28b9f379b4b091f6c0088ab
Nguyễn Thị Ngọc Huyền <henrynguyen02081997@gmail.com>
2018-06-03 21:14:47 +0700

    merge fixx

--- DTO/HangHoa.cs
+++ DTO/HangHoa.cs
@@ -8,16 +8,102 @@ namespace DTO
 {
     public class HangHoa
     {
-        public string Ten { get; set; }
-        public string Ma { get; set; }
-        public string Loai { get; set; }
-        public uint Gia { get; set; }
-        public string TenHinhAnh { get; set; }
+        private string ten;
+        private string ma;
+        private string tenHinhAnh;
+        private string loaiChiTiet;
+        private LoaiHangHoa loaiHangHoa;
+        private string loai;
+        private uint gia;
 
-        public string LoaiChiTiet { get; set; }
 
-        public LoaiHangHoa LoaiHangHoa { get; set; }
+        public string Ten
+        {
+            get
+            {
+                return ten;
+            }
 
-     
+            set
+            {
+                ten = value;
+            }
+        }
+        public string Ma
+        {
+            get
+            {
+                return ma;
+            }
+
+            set
+            {
+                ma = value;
+            }
+        }
+        public string Loai
+        {
+            get
+            {
+                return loai;
+            }
+
+            set
+            {
+                loai = value;
+            }
+        }
+        public uint Gia
+        {
+            get
+            {
+                return gia;
+            }
+
+            set
+            {
+                gia = value;
+            }
+        }
+        public string TenHinhAnh
+        {
+            get
+            {
+                return tenHinhAnh;
+            }
+
+            set
+            {
+                tenHinhAnh = value;
+            }
+        }
+
+        public string LoaiChiTiet
+        {
+            get
+            {
+                return loaiChiTiet;
+            }
+
+            set
+            {
+                loaiChiTiet = value;
+            }
+        }
+
+        public LoaiHangHoa LoaiHangHoa
+        {
+            get
+            {
+                return loaiHangHoa;
+            }
+
+            set
+            {
+                loaiHangHoa = value;
+            }
+        }
+
+
     }
 }
--- DTO/LoaiMon.cs
+++ DTO/LoaiMon.cs
@@ -8,6 +8,8 @@ namespace DTO
 {
     public class LoaiMon
     {
+        private string ma;
+        private string ten;
         public string Ma { get; set; }
         public string Ten { get; set; }
     }
