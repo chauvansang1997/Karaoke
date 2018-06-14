d3f507b467c5e7bee2d33e79996499c0e40d735e
Thanh <15520798@gm.uit.edu.vn>
2018-06-14 09:30:48 +0700

    merge with thanh frmBaoCaoLuongTong, frmBaoCaoLuong, frmPhanCong

--- Karaoke/Program.cs
+++ Karaoke/Program.cs
@@ -20,7 +20,7 @@ namespace Karaoke
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmBaoCaoLuongTong());
         }
     }
 }
