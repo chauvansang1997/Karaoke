2692b937241e8814b7821b19ae7398999fa5614c
Nguyễn Thị Ngọc Huyền <henrynguyen02081997@gmail.com>
2018-06-16 06:52:06 +0700

    Update Frame khachhang

--- Karaoke/GUI_KhachHang/frmKhachHang.Designer.cs
+++ Karaoke/GUI_KhachHang/frmKhachHang.Designer.cs
@@ -383,7 +383,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1115, 520);
+            this.ClientSize = new System.Drawing.Size(1115, 509);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.btnLastPage);
