31ecc8972d40f2a70fb366f608d3f28e32cf5ec2
sang chau van <chauvansang97@gmail.com>
2019-04-21 11:42:10 +0700

    fix connect string

---
+++ DTO/KhachHangData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class KhachHangData
+    {
+        public string Ten { get; set; }
+
+        public string SoDT { get; set; }
+
+        public string Address { get; set; }
+
+        public string Ma { get; set; }
+
+        public int LoaiKH { get; set; }
+
+        public string SDT { get; set; }
+    }
+}
