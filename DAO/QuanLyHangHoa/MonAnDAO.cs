95ac2ca3eb65579134b60efaff5f9c11954016bf
thinhphanngoc97 <15520846@gm.uit.edu.vn>
2019-04-27 08:01:19 +0700

    xong tim kiem mon an

--- DAO/QuanLyHangHoa/MonAnDAO.cs
+++ DAO/QuanLyHangHoa/MonAnDAO.cs
@@ -10,17 +10,17 @@ namespace DAO.QuanLyHangHoa
 {
     public static class MonAnDAO
     {
-        public static bool ThemMonAn(MonAn monAn,string listNguyenLieu,string listSoLuong)
+        public static bool ThemMonAn(MonAn monAn, string listNguyenLieu, string listSoLuong)
         {
-           
+
             //Thêm món ăn
             string query = " EXEC uspThemMonAn @listNguyenLieu,@listSoLuong,@tenmon,@loaimon,@anhminhhoa,@dongia";
 
 
-           
+
             //truyền tham số vào câu truy vấn
             List<SqlParameter> parameters = new List<SqlParameter>()
-            { 
+            {
                 new SqlParameter("@listNguyenLieu", SqlDbType.VarChar) { IsNullable = false, Value = listNguyenLieu },
                 new SqlParameter("@listSoLuong", SqlDbType.VarChar) { IsNullable = false, Value = listSoLuong },
                 new SqlParameter("@tenmon",SqlDbType.NVarChar){IsNullable=false,Value=monAn.Ten },
@@ -100,7 +100,7 @@ namespace DAO.QuanLyHangHoa
 
             return true;
         }
-        public static List<MonAn> XemMonAn(string tenMonAn, int loaiMonAn, int pageNumber,int pageSize,uint donGia = 0)
+        public static List<MonAn> XemMonAn(string tenMonAn, int loaiMonAn, int pageNumber, int pageSize, uint donGia = 0)
         {
             string query = "EXEC uspXemMonAn @tenMonAn,@loaiMonAn,@donGia,@pageNumber,@pageSize";
 
@@ -122,9 +122,9 @@ namespace DAO.QuanLyHangHoa
                         {
                             Ma = x[0].ToString(),
                             Ten = x[1].ToString(),
-                            Loai = x[2].ToString(),                        
+                            Loai = x[2].ToString(),
                             TenHinhAnh = x[3].ToString(),
-                             Gia = uint.Parse(x[4].ToString())
+                            Gia = uint.Parse(x[4].ToString())
                         });
             }
             catch (Exception ex)
@@ -134,45 +134,49 @@ namespace DAO.QuanLyHangHoa
 
             return list;
         }
+
         public static List<MonAnDataSource> XemMonAnDataSource(string tenMonAn, int loaiMonAn, int pageNumber, int pageSize)
         {
-            string query = "EXEC uspXemMonAn2 @tenMonAn,@loaiMonAn,@pageNumber,@pageSize";
+            KaraokeDataContext karaokeDataContext = new KaraokeDataContext();
 
-            //truyền tham số vào câu truy vấn
-            List<SqlParameter> parameters = new List<SqlParameter>()
-            {
-                new SqlParameter("@tenMonAn",SqlDbType.NVarChar){IsNullable=false,Value=tenMonAn },
-                new SqlParameter("@loaiMonAn",SqlDbType.Int){IsNullable=false,Value=loaiMonAn},
-                 new SqlParameter("@pageNumber",SqlDbType.Int){IsNullable=false,Value=pageNumber},
-                new SqlParameter("@pageSize",SqlDbType.Int){IsNullable=false,Value=pageSize },
-            };
-            List<MonAnDataSource> list = new List<MonAnDataSource>();
-            try
-            {
-                DataTable table = Dataprovider.ExcuteQuery(query, parameters.ToArray());
-                list = table.AsEnumerable().ToList().ConvertAll(x =>
-                        new MonAnDataSource()
-                        {
-                            Ma = x[0].ToString(),
-                            Ten = x[1].ToString(),
-                           MaLoaiHangHoa= x[2].ToString(),
-                           Gia= x[3].ToString(),
-                           AnhMinhHoa= x[4].ToString(),
-                           TenLoaiHangHoa= x[5].ToString(),
-                        });
-            }
-            catch (Exception ex)
-            {
-                Utility.Log(ex);
-            }
+            var listMonAn = from monan in karaokeDataContext.MONANs
+                            join loaimon in karaokeDataContext.LOAIMONANs
+                                on monan.LOAIMON equals loaimon.MA
+                            let temp1 = Utility.convertUnsign(monan.TENMON)
+                            where (temp1.Contains(Utility.convertUnsign(tenMonAn)) && (loaiMonAn == 0 ? monan.LOAIMON == monan.LOAIMON : monan.LOAIMON == loaiMonAn))
+                            select new MonAnDataSource()
+                            {
+                                Ma = monan.MAMON,
+                                Ten = monan.TENMON,
+                                MaLoaiHangHoa = loaimon.MA.ToString(),
+                                TenLoaiHangHoa = loaimon.TENLOAI.ToString(),
+                                Gia = monan.DONGIA.ToString(),
+                                AnhMinhHoa = monan.ANHMINHHOA
+                            };
 
-            return list;
+            var listMonAn2 = karaokeDataContext.MONANs.AsEnumerable().Where(s =>
+            Utility.convertUnsign(s.TENMON).Contains(Utility.convertUnsign(tenMonAn))
+
+            && (loaiMonAn == 0 ? s.LOAIMON == s.LOAIMON : s.LOAIMON == loaiMonAn)).Select(
+
+                t => new MonAnDataSource()
+                {
+                    Ma = t.MAMON,
+                    Ten = t.TENMON,
+                    MaLoaiHangHoa = t.LOAIMONAN.MA.ToString(),
+                    TenLoaiHangHoa = t.LOAIMONAN.TENLOAI.ToString(),
+                    Gia = t.DONGIA.ToString(),
+                    AnhMinhHoa = t.ANHMINHHOA
+                });
+            var temp = listMonAn2.ToList();
+            return temp;
         }
-        public static List<MonAn> XemMonAn(string tenMonAn,int loaiMonAn,uint donGia = 0)
+
+        public static List<MonAn> XemMonAn(string tenMonAn, int loaiMonAn, uint donGia = 0)
         {
             string query = "EXEC uspXemMonAn @tenMonAn,@loaiMonAn,@donGia";
 
- 
+
             //truyền tham số vào câu truy vấn
             List<SqlParameter> parameters = new List<SqlParameter>()
             {
@@ -184,16 +188,16 @@ namespace DAO.QuanLyHangHoa
             List<MonAn> list = null;
             try
             {
-               DataTable table = Dataprovider.ExcuteQuery(query, parameters.ToArray());
-               list = table.AsEnumerable().ToList().ConvertAll(x =>
-                       new MonAn()
-                       {
-                           Ma = x[0].ToString(),
-                           Ten = x[1].ToString(),
-                           Loai = x[2].ToString(),
-                           Gia = uint.Parse(x[3].ToString()),
-                           TenHinhAnh = x[4].ToString()
-                       });
+                DataTable table = Dataprovider.ExcuteQuery(query, parameters.ToArray());
+                list = table.AsEnumerable().ToList().ConvertAll(x =>
+                        new MonAn()
+                        {
+                            Ma = x[0].ToString(),
+                            Ten = x[1].ToString(),
+                            Loai = x[2].ToString(),
+                            Gia = uint.Parse(x[3].ToString()),
+                            TenHinhAnh = x[4].ToString()
+                        });
             }
             catch (Exception ex)
             {
@@ -237,7 +241,7 @@ namespace DAO.QuanLyHangHoa
             int count = 0;
             try
             {
-                count=int.Parse(Dataprovider.ExcuteScalar(query, parameters.ToArray()).ToString());
+                count = int.Parse(Dataprovider.ExcuteScalar(query, parameters.ToArray()).ToString());
             }
             catch (Exception ex)
             {
@@ -287,7 +291,7 @@ namespace DAO.QuanLyHangHoa
                             Ten = x[1].ToString(),
                             DonViTinh = x[2].ToString(),
                             Gia = x[3].ToString(),
-                            SoLuong= x[4].ToString(),
+                            SoLuong = x[4].ToString(),
                         });
             }
             catch (Exception ex)
--- DTO/Utility.cs
+++ DTO/Utility.cs
@@ -116,5 +116,49 @@ namespace DTO
         {
             File.WriteAllText("crashlog" + DateTime.Now.ToString(@"dd\/MM\/yyyy HH:mm") + ".txt",  msg);
         }
+
+        private static readonly string[] VietnameseSigns = new string[]
+        {
+
+            "aAeEoOuUiIdDyY",
+
+            "áàạảãâấầậẩẫăắằặẳẵ",
+
+            "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
+
+            "éèẹẻẽêếềệểễ",
+
+            "ÉÈẸẺẼÊẾỀỆỂỄ",
+
+            "óòọỏõôốồộổỗơớờợởỡ",
+
+            "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ",
+
+            "úùụủũưứừựửữ",
+
+            "ÚÙỤỦŨƯỨỪỰỬỮ",
+
+            "íìịỉĩ",
+
+            "ÍÌỊỈĨ",
+
+            "đ",
+
+            "Đ",
+
+            "ýỳỵỷỹ",
+
+            "ÝỲỴỶỸ"
+        };
+
+        public static string convertUnsign(string str)
+        {
+            for (int i = 1; i < VietnameseSigns.Length; i++)
+            {
+                for (int j = 0; j < VietnameseSigns[i].Length; j++)
+                    str = str.Replace(VietnameseSigns[i][j], VietnameseSigns[0][i - 1]);
+            }
+            return str;
+        }
     }
 }
