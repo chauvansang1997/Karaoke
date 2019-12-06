241bbe80dfe03719fde2f56d791c250f19582543
danhthanh418 <15520798@gm.uit.edu.vn>
2019-12-06 21:50:17 +0700

    fix sql connection string to sqlexpress

--- DAO/Properties/Settings.Designer.cs
+++ DAO/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace DAO.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=dbQuanLyKaraoke;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLExpress;Initial Catalog=dbQuanLyKaraoke;Integrated Security=True")]
         public string dbQuanLyKaraokeConnectionString {
             get {
                 return ((string)(this["dbQuanLyKaraokeConnectionString"]));
